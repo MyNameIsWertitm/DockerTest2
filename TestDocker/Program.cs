f3ec1153aa17f258bba920061313391066e98a4a
Ivan Pantyukhov <vano210503@gmail.com>
2025-12-02 15:56:01 +0300

    add logger

--- TestDocker/Program.cs
+++ TestDocker/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (ILogger<Program> _logger) => { _logger.LogInformation("logging Hello World!"); return "Hello World!"; });
 
 app.Run();
