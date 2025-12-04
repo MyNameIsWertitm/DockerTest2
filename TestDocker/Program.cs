663cbd774aed281a77c6420a01f3ac6a0f186d6d
Ivan Pantyukhov <vano210503@gmail.com>
2025-12-04 11:30:48 +0300

    add dotenv.net nuget

--- TestDocker/Program.cs
+++ TestDocker/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using dotenv.net;
 
-app.MapGet("/", (ILogger<Program> _logger) => { _logger.LogInformation("logging Hello World!"); return "Hello World!"; });
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+string envPath = builder.Configuration["EnvPath"];
+DotEnv.Load(options: new DotEnvOptions(envFilePaths: new[] { envPath }));
+
+WebApplication app = builder.Build();
+
+app.MapGet("/", (ILogger<Program> _logger) => 
+{
+    var envVar = DotEnv.Read();
+    _logger.LogInformation("logging Hello World!"); 
+    return $"{envVar["APP_NAME"]}"; 
+});
 
 app.Run();
